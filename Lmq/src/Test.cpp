d09f3460c47a4888cddc61509b8af4553146499c
lienminhquang <=>
2019-07-14 23:13:49 +0700

    + init

---
+++ Lmq/src/Test.cpp
@@ -0,0 +1,10 @@
+#include "Test.h"
+#include <stdio.h>
+
+namespace Lmq
+{
+	void Print() 
+	{
+		printf("Hello form Lmq engine!");
+	}
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,9 @@
+
+namespace Lmq {
+	__declspec(dllimport) void Print();
+}
+
+void main()
+{
+	Lmq::Print();
+}
